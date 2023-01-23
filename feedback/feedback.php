c990cf48a0c871aea95c0dc531675ad625b2345c
Akam Programist <akadev404@gmail.com>
2023-01-24 00:35:28 +0300

    commits

--- feedback/feedback.php
+++ feedback/feedback.php
@@ -37,7 +37,7 @@ if (isset($_GET['delete'])) {
             </div>
         </div>
         <a href="feedback.php?delete=<?php echo $item['id']; ?>" class="btn btn-danger">Delete</a>
-        <a href="edit.php?edit=<?php echo $item['id']; ?>" class="btn btn-info">edit</a>
+        <a href="edit.php?edit=<?php echo $item['id']; ?>" class="btn text-white btn-primary">edit</a>
     </div>
 <?php endforeach; ?>
 <?php include 'inc/footer.php'; ?>
