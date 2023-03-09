07effce6e2996e6c094c7307ef94c617d09d5f18
Akam Programist <akadev404@gmail.com>
2023-03-09 22:17:14 +0300

    commits

--- feedback/config/database.php
+++ feedback/config/database.php
@@ -1,8 +1,8 @@
 <?php
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'akam');
-define('DB_PASS', 'akam');
+define('DB_USER', 'root');
+define('DB_PASS', '');
 define('DB_NAME', 'php_dev');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
